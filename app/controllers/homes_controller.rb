e54aa3e07c3865bc2c39cb7edb1b698af07b15f8
Marc Marino <marc.a.marino@gmail.com>
2018-11-27 06:48:20 -0500

    corrected typos and cleaned up constants.rb file to more clearly define calculations

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -4,15 +4,16 @@ class HomesController < ApplicationController
     @abv=params[:abv].to_f
     @volume=params[:volume].to_f
     @unit=params[:unit]
-    @cost=(params[:cost].to_f)
+    @cost=params[:cost].to_f
+    @packaging=params[:packaging]
 
     @number_of_servings = Constants.calculate_servings(@abv, @volume, @unit)
     @cost_per_serving = Constants.calculate_cost_per_serving(@cost, @number_of_servings)
 
     @wine_cost = Constants.calculate_wine_cost(@cost_per_serving)
     @spirits_cost = Constants.calculate_spirits_cost(@cost_per_serving)
-    @beer_6pk_cost = Constants.calculate_6pk_of_beer_cost(@cost_per_serving)
-    @beer_4pk_cost = Constants.calculate_4pk_of_beer_cost(@cost_per_serving)
+    @beer_6pk_cost = Constants.calculate_6pk_12oz_of_beer_cost(@cost_per_serving)
+    @beer_4pk_cost = Constants.calculate_4pk_16oz_of_beer_cost(@cost_per_serving)
 
   end
 
